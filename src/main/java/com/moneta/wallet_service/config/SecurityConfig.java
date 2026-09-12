d0dfab1254e16b810fb9d91180f66c1169c455ff
Furkan Güven <furkanguven.dev@gmail.com>
2026-09-12 22:21:59 +0300

    feat: health update

--- src/main/java/com/moneta/wallet_service/config/SecurityConfig.java
+++ src/main/java/com/moneta/wallet_service/config/SecurityConfig.java
@@ -75,6 +75,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/health").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
